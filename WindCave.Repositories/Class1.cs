42e197310a46366b260df68bf01651ac6f4f6afd
Ashan Madushanka <ashan_m@3logixs.lk>
2020-05-05 22:59:53 +0530

    Add project files.

---
+++ WindCave.Repositories/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WindCave.Repositories
+{
+    public class Class1
+    {
+    }
+}
---
+++ WindCave.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WindCave.Services
+{
+    public class Class1
+    {
+    }
+}
