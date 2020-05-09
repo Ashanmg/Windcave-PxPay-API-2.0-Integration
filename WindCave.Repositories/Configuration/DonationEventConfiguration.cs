ddc1feea04223962b485b7b711380ac6dd74cc74
Ashan Madushanka <ashan_m@3logixs.lk>
2020-05-09 22:28:59 +0530

    Create configurations

---
+++ WindCave.Repositories/Configuration/DonationEventConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windcave.Model;
+
+namespace WindCave.Data.Configuration
+{
+    public class DonationEventConfiguration
+    {
+    }
+}
